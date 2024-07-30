1589af63c922141e70e5c51f0b3760adac202109
Arvind Gupta <88265647+Guptagreat@users.noreply.github.com>
2024-07-30 12:36:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let menuIcon = document.querySelector('#menu-icon');
+
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+
+menuIcon.classList.toggle('bx-x')
+
+navbar.classList.toggle('active');
+
+}
